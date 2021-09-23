ede97fc8cb2c90148808eb61628263c9c2d42519
刘梦岗 <1138075190@qq.com>
2021-09-24 07:26:05 +0800

    包的指令练习和3d特效的源码

---
+++ 第八阶段/包管理器/指令的应用/index.js
@@ -0,0 +1,3 @@
+console.log('我被执行了');
+console.log('安装的包，但包里面有指令可以是用；npx 指令执行；如：npx nodemon index.js');
+console.log("可以在package.json文件的secipt中填写指令；比如常见的npm run dev；我们自己也可添加，比如我添加的：'go':'npx nodemon index.js';然后就可以在控制台运行npm run go了；");
