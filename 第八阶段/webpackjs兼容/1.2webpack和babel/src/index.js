46e9265153923ab0be146824702e5d8c2f88d75b
刘梦岗 <1138075190@qq.com>
2021-10-21 08:06:00 +0800

    'webpack和babel的结合'

---
+++ 第八阶段/webpackjs兼容/1.2webpack和babel/src/index.js
@@ -0,0 +1,15 @@
+console.log(132)
+const abc = () => {
+    console.log(4155);
+}
+abc()
+
+const promise = new Promise(resolve => {
+    resolve(2)
+})
+console.log(promise);
+async function getName() {
+    const info = await 1
+    console.log(info);
+}
+getName()
---
+++ 第八阶段/webpackjs兼容/1.2webpack和babel/webpack.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    mode: "development",
+    devtool: "source-map",
+    module: {
+        rules: [{
+            test: /\.js$/,
+            use: "babel-loader"
+        }]
+    }
+}
