a146afa7e021c4e85c58f97a0d6ebd3f091f34e0
刘梦岗 <1138075190@qq.com>
2021-12-01 22:16:16 +0800

    'vite和vue3的初体验'

---
+++ vue3.0/vue3-app-vite/src/components/HelloWorld.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    {{ count }}
+    <button @click="increase">increase</button>
+    <button @click="decrease">decrease</button>
+  </div>
+</template>
+
+<script>
+import { ref, watchEffect } from "vue";
+function countInfo() {
+  let count = ref(0);
+  function increase() {
+    count.value++;
+  }
+  function decrease() {
+    count.value--;
+  }
+  watchEffect(() => {
+    console.log(555);
+    console.log(count.value);
+  });
+  return {
+    count,
+    increase,
+    decrease,
+  };
+}
+export default {
+  setup() {
+    // let count = ref(0);
+    // function increase() {
+    //   count.value++;
+    // }
+    // window.count = count
+    // function decrease() {
+    //   count.value--;
+    // }
+    // watchEffect(() => {
+    //   console.log(555);
+    //   console.log(count.value);
+    // });
+    return {
+      ...countInfo()
+    };
+  },
+};
+</script>
