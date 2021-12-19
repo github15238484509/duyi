46d4d4cabbed70cdb2065e74a2a028d976de5b06
刘梦岗 <1138075190@qq.com>
2021-12-19 20:19:08 +0800

    '移动端架子的搭建'

---
+++ vue2.0/mobile-app/src/main.js
@@ -0,0 +1,18 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import "lib-flexible"
+
+import Vant from "vant"
+import "vant/lib/index.css"
+Vue.use(Vant)
+
+
+Vue.config.productionTip = false
+
+new Vue({
+    router,
+    store,
+    render: h => h(App)
+}).$mount('#app')
---
+++ vue2.0/mobile-app/vue.config.js
@@ -0,0 +1,12 @@
+const px2rem = require("postcss-px2rem")
+module.exports = {
+    css: {
+        loaderOptions: {
+            postcss: {
+                plugins: [px2rem({
+                    remUnit: 75
+                })]
+            }
+        }
+    }
+}
