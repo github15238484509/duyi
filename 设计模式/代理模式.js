eccecb3baa2d3067cd8fae78ea296a39cbe022cc
lmg <1138075190@qq.com>
2022-02-19 13:16:03 +0800

    '代理模式缓存模式'

--- 设计模式/代理模式.js
+++ 设计模式/代理模式.js
@@ -100,14 +100,102 @@
 
 ;
 (() => {
-    class LazyImage {
-        constructor(initialImag, originImage, dom = document.body) {
-            this.initialImag = initialImag
-            this.originImage = originImage
-            this.init()
+    function CreateImage(dom, src) {
+        this.img = document.createElement("img")
+        this.dom = dom
+        this.img.src = src
+        this.dom.appendChild(this.img)
+        this.setSrc = function (url) {
+            this.img.src = url
+            this.app()
         }
-        init() {
+        this.app = function () {
+            this.dom.appendChild(this.img)
+        }
+        this.setSrc(src)
+    }
+    var perch = "https://img-blog.csdnimg.cn/2020021311390278.png?x-oss-process=image/watermark,type_ZmFuZ3poZW5naGVpdGk,shadow_10,text_aHR0cHM6Ly9ibG9nLmNzZG4ubmV0L3FxXzM5ODYxNDQx,size_16,color_FFFFFF,t_70"
 
+    class ProxyImage {
+        constructor(url, proxyFun) {
+            var img = new Image()
+            img.onload = function () {
+                setTimeout(() => {
+                    proxyFun.setSrc(this.src)
+                }, 1000)
+            }
+            img.src = url
+        }
+    }
+    var origin = "https://img2.baidu.com/it/u=1034746523,1428781265&fm=253&fmt=auto&app=138&f=JPEG?w=1134&h=500"
+    // new ProxyImage(origin, new CreateImage(imgDom, perch))
+})()
+
+
+;
+(() => {
+    var btns = document.querySelectorAll("button")
+    var target = document.querySelector("#imgDom")
+    console.log(btns);
+
+    function random(start, end) {
+        return Math.floor(Math.random() * (end - start)) + start
+    }
+
+    // abcdef
+    function randomColor() {
+        var arr = ["a", "b", "c", "d", "e", "f"]
+        var str = "#"
+        for (let i = 0; i < 6; i++) {
+            [i];
+            str += arr[random(0, 6)]
+        }
+        return str
+    }
+
+
+    function getStyle(type) {
+        if (type === "border") {
+            return ["border", `${random(0,10)}px solid ${randomColor()}`]
+        } else if (type === "color") {
+            return ["color", randomColor()]
+        } else if (type === "background") {
+            return ["backgroundColor", randomColor()]
+        } else if (type === "width") {
+            return ["width", `${random(100,200)}px`]
+        } else if (type === "height") {
+            return ["height", `${random(100,200)}px`]
+        }
+    }
+
+
+    function ProxyStyle(fn, time = 300) {
+        var timer = null;
+        var styles = new Map()
+        return function () {
+            this.set = function (key, value) {
+                styles.set(key, value)
+                this.render()
+            }
+            this.render = function () {
+                clearTimeout(timer)
+                timer = setTimeout(() => {
+                    styles.forEach((e, a) => {
+                        fn.call(this, e)
+                    })
+                    styles.clear()
+                }, time)
+            }
+        }
+    }
+
+    function setinfo(tyle) {
+        target.style[tyle[0]] = tyle[1]
+    }
+    var proxystyle = new(ProxyStyle(setinfo, 1000))()
+    for (const item of btns) {
+        item.onclick = function () {
+            proxystyle.set(getStyle(this.dataset.tyle)[0], getStyle(this.dataset.tyle))
         }
     }
 })()
