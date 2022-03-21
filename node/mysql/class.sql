8470e1f49c3beb44550d617f27a5daf3af1ddf16
刘梦岗 <1138075190@qq.com>
2022-03-21 16:57:47 +0800

    'sql练习'

--- node/mysql/class.sql
+++ node/mysql/class.sql
@@ -1,7 +1,7 @@
 /*
  Navicat Premium Data Transfer
 
- Source Server         : 在线测试
+ Source Server         : 练习
  Source Server Type    : MySQL
  Source Server Version : 80019
  Source Host           : localhost:3306
@@ -11,7 +11,7 @@
  Target Server Version : 80019
  File Encoding         : 65001
 
- Date: 20/03/2022 17:13:29
+ Date: 21/03/2022 16:56:30
 */
 
 SET NAMES utf8mb4;
@@ -28,13 +28,15 @@ CREATE TABLE `class`  (
   PRIMARY KEY (`id`) USING BTREE,
   INDEX `id`(`teacher`) USING BTREE,
   CONSTRAINT `id` FOREIGN KEY (`teacher`) REFERENCES `teacher` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Records of class
 -- ----------------------------
 INSERT INTO `class` VALUES (1, '班级1', 1);
 INSERT INTO `class` VALUES (2, '班级2', 1);
+INSERT INTO `class` VALUES (4, '班级6', 1);
+INSERT INTO `class` VALUES (5, '班级5', 1);
 
 -- ----------------------------
 -- Table structure for teacher
@@ -46,11 +48,26 @@ CREATE TABLE `teacher`  (
   `sex` bit(1) NOT NULL DEFAULT b'0' COMMENT '老师的性别',
   `birth` date NOT NULL COMMENT '老师的出生日期',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Records of teacher
 -- ----------------------------
 INSERT INTO `teacher` VALUES (1, '刘梦岗', b'1', '2022-03-20');
+
+-- ----------------------------
+-- Table structure for test
+-- ----------------------------
+DROP TABLE IF EXISTS `test`;
+CREATE TABLE `test`  (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of test
+-- ----------------------------
+INSERT INTO `test` VALUES (4, '132');
 
 SET FOREIGN_KEY_CHECKS = 1;
