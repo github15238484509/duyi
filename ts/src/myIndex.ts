f663cba54c93a17c03d8dd974d8e8dff4d0e5acb
刘梦岗 <1138075190@qq.com>
2022-04-16 17:07:00 +0800

    'ts的导入导出'

---
+++ ts/src/myIndex.ts
@@ -0,0 +1,4 @@
+import { add, name } from "./myModule";
+
+console.log(add(1,2))
+console.log(name);
---
+++ ts/src/myModule.ts
@@ -0,0 +1,4 @@
+export const name:string="lmg"
+export function add(a:number,b:number):number{
+  return a+b
+}
