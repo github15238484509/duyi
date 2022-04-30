bf29c10bc2bf7ad06b131ac6b8c07bdd646f1a54
刘梦岗 <1138075190@qq.com>
2022-04-30 15:17:38 +0800

    '图片防盗链'

--- node/mysql/sequelize/init.js
+++ node/mysql/sequelize/init.js
@@ -4,6 +4,16 @@ const path = require("path")
 const app = exporess()
 const public = path.resolve(__dirname, "public")
 var cookieParser = require('cookie-parser')
+let url = require("url")
+//  图片防盗链
+app.use((req, res, next) => {
+    let host = req.headers.host;
+    let referer = req.headers.referer;
+    if (referer && new URL(req.headers.referer).host !== host) {
+        req.url = "upload/water/1651292342870_0jr8w9.png.png"
+    }
+    next()
+})
 
 //静态资源服务器
 app.use(exporess.static(public))
@@ -22,9 +32,9 @@ app.use("/api/students", require("./router/students"))
 //管理员的接口
 app.use("/api/admin", require("./router/api/admin"))
 //上传文件
-app.use("/api/upload",require("./router/api/upload"))
+app.use("/api/upload", require("./router/api/upload"))
 //下载文件
-app.use("/api/download",require("./router/api/download"))
+app.use("/api/download", require("./router/api/download"))
 app.listen(3000, () => {
-  console.log("localhost:3000");
+    console.log("localhost:3000");
 })
